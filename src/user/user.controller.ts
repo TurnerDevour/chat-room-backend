cde267a96c1dc2bd0f45c0712be48e71aebee81d
liuzhengzhi <turneratc@163.com>
2025-12-19 17:54:01 +0800

    feat: implement JWT authentication and user management features

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Post, Body, Get, Query, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Query,
+  Inject,
+  BadRequestException,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { EmailService } from '../email/email.service';
 import { RedisService } from '../redis/redis.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { RequireLogin, UserInfo } from '../custom.decorator';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
+import { UpdateUserDto } from './dto/udpate-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -13,6 +26,9 @@ export class UserController {
 
   @Inject(RedisService)
   private redisService: RedisService;
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
 
   @Get('register-captcha')
   async captcha(@Query('address') address: string) {
@@ -28,8 +44,88 @@ export class UserController {
     return '发送成功';
   }
 
+  @Get('update_password/captcha')
+  async updatePasswordCaptcha(@Query('address') address: string) {
+    if (!address) {
+      throw new BadRequestException('邮箱地址不能为空');
+    }
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_password_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改密码验证码',
+      html: `<p>你的更改密码验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
+  @Get('update/captcha')
+  async updateCaptcha(@Query('address') address: string) {
+    if (!address) {
+      throw new BadRequestException('邮箱地址不能为空');
+    }
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_user_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改用户信息验证码',
+      html: `<p>你的验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
   @Post('register')
   async register(@Body() registerUser: RegisterUserDto) {
     return this.userService.register(registerUser);
+  }
+
+  @Post('login')
+  async userLogin(@Body() loginUser: LoginUserDto) {
+    const user = await this.userService.login(loginUser);
+
+    return {
+      user,
+      token: this.jwtService.sign(
+        {
+          userId: user.id,
+          username: user.username,
+        },
+        {
+          expiresIn: '7d',
+        },
+      ),
+    };
+  }
+
+  @Get('info')
+  @RequireLogin()
+  async info(@UserInfo('userId') userId: number) {
+    return this.userService.findUserDetailById(userId);
+  }
+
+  @Post('update_password')
+  async updatePassword(@Body() passwordDto: UpdateUserPasswordDto) {
+    return this.userService.updatePassword(passwordDto);
+  }
+
+  @Post('update')
+  @RequireLogin()
+  async update(
+    @UserInfo('userId') userId: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.update(userId, updateUserDto);
   }
 }
